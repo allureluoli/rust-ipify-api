94a933379fc49728959c25928b13846f46e5f6f2
3345483363@qq.com <3345483363@qq.com>
2025-03-02 13:34:07 +0800

    初始化

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn index(req: HttpRequest) -> impl Responder {
+    if let Some(peer_addr) = req.connection_info().peer_addr() {
+        format!("{}", peer_addr)
+    } else {
+        "Could not determine client IP".to_string()
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
